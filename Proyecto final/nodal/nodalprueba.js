49e4e0b92c9c1ca850636e6e16ea3103f45abf70
Pedro Tumay R <100743285+EiveenL@users.noreply.github.com>
2022-11-17 18:58:03 -0500

    Add files via upload

---
+++ Proyecto final/nodal/nodalprueba.js
@@ -0,0 +1,158 @@
+const openModal1 = document.querySelector('.herocta1');
+const modal1 = document.querySelector('.modal1');
+const closeModal1 = document.querySelector('.modalclose1');
+
+openModal1.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal1.classList.add('modal--show');
+});
+
+closeModal1.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal1.classList.remove('modal--show');
+});
+
+const openModal2 = document.querySelector('.herocta2');
+const modal2 = document.querySelector('.modal2');
+const closeModal2 = document.querySelector('.modalclose2');
+
+openModal2.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal2.classList.add('modal--show');
+});
+
+closeModal2.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal2.classList.remove('modal--show');
+});
+
+const openModal3 = document.querySelector('.herocta3');
+const modal3 = document.querySelector('.modal3');
+const closeModal3 = document.querySelector('.modalclose3');
+
+openModal3.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal3.classList.add('modal--show');
+});
+
+closeModal3.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal3.classList.remove('modal--show');
+});
+const openModal4 = document.querySelector('.herocta4');
+const modal4 = document.querySelector('.modal2');
+const closeModal4 = document.querySelector('.modalclose4');
+
+openModal4.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal4.classList.add('modal--show');
+});
+
+closeModal4.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal4.classList.remove('modal--show');
+});
+const openModal5 = document.querySelector('.herocta5');
+const modal5 = document.querySelector('.modal5');
+const closeModal5 = document.querySelector('.modalclose5');
+
+openModal5.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal5.classList.add('modal--show');
+});
+
+closeModal5.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal5.classList.remove('modal--show');
+});
+const openModal6 = document.querySelector('.herocta6');
+const modal6 = document.querySelector('.modal6');
+const closeModal6 = document.querySelector('.modalclose6');
+
+openModal6.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal6.classList.add('modal--show');
+});
+
+closeModal6.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal6.classList.remove('modal--show');
+});
+const openModal7 = document.querySelector('.herocta7');
+const modal7 = document.querySelector('.modal7');
+const closeModal7 = document.querySelector('.modalclose7');
+
+openModal7.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal7.classList.add('modal--show');
+});
+
+closeModal7.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal7.classList.remove('modal--show');
+});
+const openModal8 = document.querySelector('.herocta8');
+const modal8 = document.querySelector('.modal8');
+const closeModal8 = document.querySelector('.modalclose8');
+
+openModal8.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal8.classList.add('modal--show');
+});
+
+closeModal8.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal8.classList.remove('modal--show');
+});
+const openModal9 = document.querySelector('.herocta9');
+const modal9 = document.querySelector('.modal9');
+const closeModal9 = document.querySelector('.modalclose9');
+
+openModal9.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal9.classList.add('modal--show');
+});
+
+closeModal9.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal9.classList.remove('modal--show');
+});
+const openModal10 = document.querySelector('.herocta10');
+const modal10 = document.querySelector('.modal10');
+const closeModal10 = document.querySelector('.modalclose10');
+
+openModal10.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal10.classList.add('modal--show');
+});
+
+closeModal10.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal10.classList.remove('modal--show');
+});
+const openModal11 = document.querySelector('.herocta11');
+const modal11 = document.querySelector('.modal11');
+const closeModal11 = document.querySelector('.modalclose11');
+
+openModal11.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal11.classList.add('modal--show');
+});
+
+closeModal11.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal11.classList.remove('modal--show');
+});
+const openModal12 = document.querySelector('.herocta12');
+const modal12 = document.querySelector('.modal12');
+const closeModal12 = document.querySelector('.modalclose12');
+
+openModal12.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal12.classList.add('modal--show');
+});
+
+closeModal12.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal12.classList.remove('modal--show');
+});
---
+++ Proyecto final/reg/dbcitamedica.sql
@@ -0,0 +1,64 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Base de datos: `dbcitamedica`
+--
+CREATE DATABASE IF NOT EXISTS `dbfichamedica` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `dbfichamedica`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reg_user`
+--
+
+CREATE TABLE `reg_user` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(10) NOT NULL,
+  `apellido` varchar(10) NOT NULL,
+  `edad` varchar(10) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `correo` varchar(20) NOT NULL,
+  `telefono` int(12) NOT NULL,
+  `ssalud` varchar(20) NOT NULL,
+  `alergico` varchar(20) NOT NULL,
+  `gruposan` varchar(10) NULL,
+  `observaciones` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `reg_user`
+--
+
+INSERT INTO `reg_user` (`id`, `nombre`, `apellido`, `edad`, `sexo`, `correo`, `telefono`, `ssalud`, `alergico`, `gruposan`, `observaciones`) VALUES
+(1, 'Jus', 'Coder', '19', 'Hombre', 'jusc@info.com', 963258741, 'Particular', 'Otros', 'A+', 'campo de texto...'),
+(2, 'Pepe', 'Cruz', '20', 'Hombre', 'pep@info.com', 953862471, 'Particular', 'Otros', 'AB+', 'Ninguna...');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `reg_user`
+--
+ALTER TABLE `reg_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `reg_user`
+--
+ALTER TABLE `reg_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
